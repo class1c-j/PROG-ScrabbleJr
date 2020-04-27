9ba1b6768ea57651cf6ea8628fcc8310231a8f68
jsednem <ritam.mendes@gmail.com>
2020-04-27 01:13:44 +0100

    basic file functionality, minor fixes/cleanup, constructor from file

--- BoardBuilder/Board.cpp
+++ BoardBuilder/Board.cpp
@@ -101,7 +101,6 @@ void Board::showBoard() {
 
 
 // coords -> {linha, coluna} -> {maiuscula, minuscula}
-<<<<<<< HEAD
 void Board::insertWord(const std::string &word, std::pair<char, char> coords, const char &orientation) {
 
     //std::string pos = std::string() + (char) (coords.first + 65) + (char) (coords.second + 97);
@@ -116,20 +115,6 @@ void Board::insertWord(const std::string &word, std::pair<char, char> coords, co
     } else if (orientation == 'H') {  // linha é sempre a mesma, apenas iterar a coluna
         for (int i = coords.second; i < coords.second + word.size(); i++) {
             board.at(coords.first).at(i) = word.at(i - coords.second);
-=======
-void Board::insertWord(const std::string& word, std::pair<char, char> coords, const char& orientation) {
-    bool available = verifyBoard(word, coords, orientation);
-    if (available) {
-        if (orientation == 'V') {  // a coluna é sempre a mesma, apenas iterar a linha
-            for (int i = coords.first; i < coords.first + word.size(); i++) {
-                board.at(i).at(coords.second) = word.at(i - coords.first);
-            }
-        }
-        else if (orientation == 'H') {  // linha é sempre a mesma, apenas iterar a coluna
-            for (int i = coords.second; i < coords.second + word.size(); i++) {
-                board.at(coords.first).at(i) = word.at(i - coords.second);
-            }
->>>>>>> 2f1f47abd84743f9cb35fcc1bc7c84536c8bfa5c
         }
     }
 }
@@ -146,7 +131,6 @@ std::vector<char> Board::getCol(unsigned int col) {
     return column;
 }
 
-<<<<<<< HEAD
 
 bool Board::verifyWord(const std::string &word, std::pair<char, char> coords, const char &dir) {
 
@@ -186,42 +170,4 @@ bool Board::verifyWord(const std::string &word, std::pair<char, char> coords, co
     }
 
     return check;
-=======
-bool Board::verifyBoard(const std::string& word, std::pair<char, char> coords, const char& orientation) {
-    if (orientation == 'H') {
-        if ((numCols - coords.second) < word.size()) {
-            std::cout << "The inserted word does not respect the dimensions of the board!" << std::endl;
-            return false;
-        }
-        else {
-            std::vector<char> line = getLine(coords.first);
-            for (int i = coords.second; i <= word.size(); i++) {
-                if (line.at(i) != '0') {
-                    if (word.at(i - coords.second) != line.at(i)) {
-                        std::cout << "The inserted word does not respect the words alrealdy inserted in the board!" << std::endl;
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    else {
-        if ((numLines - coords.first) < word.size()) {
-            std::cout << "The inserted word does not respect the dimensions of the board!" << std::endl;
-            return false;
-        }
-        else {
-            std::vector<char> col = getCol(coords.second);
-            for (int i = coords.first; i <= word.size(); i++) {
-                if (col.at(i) != '0') {
-                    if (word.at(i - coords.first) != col.at(i)) {
-                        std::cout << "The inserted word does not respect the words alrealdy inserted in the board!" << std::endl;
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    return true;
->>>>>>> 2f1f47abd84743f9cb35fcc1bc7c84536c8bfa5c
 }
--- BoardBuilder/main.cpp
+++ BoardBuilder/main.cpp
@@ -26,7 +26,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     //std::ifstream f1("test1.txt");
     //testing
-<<<<<<< HEAD
     //Board Board(f1);
     //Board.insertWord("Sorocoffo", {0, 3}, 'V');  //AdV
     //Board.insertWord("Sardou", {2, 6}, 'H');    //CgH
@@ -36,18 +35,5 @@ int main() {
 
     createBoard();
 
-=======
-    Board Board(10, 15);
-    Board.insertWord("Sorocoffo", {0, 3}, 'V');  //AdV
-    Board.insertWord("Sardou", {2, 6}, 'H');    //CgH
-    Board.insertWord("Canss", {1, 14}, 'V');  //BoV
-    Board.insertWord("semislindosuwu", { 0, 2 }, 'V'); //ERRO -> tamanho da palavra ultrapssa o tamanho do tabuleiro
-    Board.insertWord("Jaszito", { 1, 7 }, 'V'); //BhV
-    Board.insertWord("Erro", { 0, 2 }, 'H'); //ERRO -> palavra inserida perturba as palavra já inseridas no tabuleiro
-    Board.insertWord("ocanaselindo", { 0, 9 }, 'H'); //ERRO -> tamanho da palavra ultrapssa o tamanho do tabuleiro
-    Board.insertWord("uwu", { 1, 11 }, 'V');//ERRO -> palavra inserida perturba as palavra já inseridas no tabuleiro
-    Board.insertWord("socuerro", { 8, 2 }, 'H'); //IcH
-    Board.showBoard();
->>>>>>> 2f1f47abd84743f9cb35fcc1bc7c84536c8bfa5c
     return 0;
 }
