efc07fa8407faafeb4611e26e8806701d5009ac7
Adelaide Miranda Santos <64143775+kat-maq@users.noreply.github.com>
2020-05-18 18:05:14 +0100

    instruções arranjadinhas

--- ScrabbleJr/main.cpp
+++ ScrabbleJr/main.cpp
@@ -100,7 +100,7 @@ void showInstructions() {
                  " of the board's words with them\nIf, during their turn, the player has 2 tiles that can be placed,"
                  "they place the tiles and\ndraw 2 from the pool.\nIf they only have 1 tile that can be played, they "
                  "place it and draw 1 tile from the pool.\nIn case the player can not play any of their tiles,"
-                 "they will choose 2 of their tiles to\nexchange for new\nones in the pool.\n\nYou can place your tiles"
+                 "they will choose 2 of their tiles to\nexchange for new ones in the pool.\n\nYou can place your tiles"
                  " in the places that have a corresponding letter only if it is the\nfirst open letter in spelling order."
                  "\n\nThe game ends when every word in the board has been finished and the winner is the player\n who "
                  "has the highest score from completing words\n\nTo play with this implementation, every time you get"
